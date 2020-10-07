f9ac07ea2f574ade5cf70e405b6cce38dccdbc7d
Yibizo <57368067+Yibizo@users.noreply.github.com>
2020-10-06 19:07:19 -0500

    Add files via upload

---
+++ javascript_files/all.js
@@ -0,0 +1,8 @@
+var menuBlock = '<div id="menuList">'+
+'<span><a href="main.html" class="linkMenu" id="mainmenu">HOME</a></span>&nbsp;&nbsp;&nbsp;'+
+'<span><a href="about.html" class="linkMenu" id="aboutmenu">ABOUT</a></span>&nbsp;&nbsp;&nbsp;'+
+'<span><a href="download.html" class="linkMenu" id="downloadmenu">DOWNLOAD</a></span>&nbsp;&nbsp;&nbsp;'+
+'<span><a href="support.html" class="linkMenu" id="supportmenu">SUPPORT</a></span>&nbsp;&nbsp;&nbsp;'+
+'</div>'
+
+document.getElementById("menu").innerHTML = menuBlock
